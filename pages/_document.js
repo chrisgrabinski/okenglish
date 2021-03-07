6fb322e2fd093351e6d2e9b0bbfa58c15b5fa958
Chris Grabiński <chris@remote.com>
2021-03-07 16:26:22 +0000

    chore: added language attribute

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="pl">
         <Head />
         <body className="bg-secondary-50 font-sans text-primary-900">
           <Main />
