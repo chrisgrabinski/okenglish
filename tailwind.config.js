1a133dd9624a40d164d048f3234a30f8c77ae1b4
Chris Grabiński <chris@remote.com>
2021-03-07 16:25:25 +0000

    chore: added brand styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.violet,
+        secondary: colors.amber,
+      },
+      fontFamily: {
+        sans: ["Fira Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
