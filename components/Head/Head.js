2cb548e75e364c902bf49ffadcdfc07603936c6c
Chris Grabiński <chris@remote.com>
2021-03-07 17:41:04 +0000

    chore: added brand font

---
+++ components/Head/Head.js
@@ -0,0 +1,15 @@
+import NextHead from "next/head";
+
+export default function Head() {
+  return (
+    <>
+      <NextHead>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,500;0,700;1,400;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </NextHead>
+    </>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import "tailwindcss/tailwind.css";
 
+import Head from "components/Head";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
