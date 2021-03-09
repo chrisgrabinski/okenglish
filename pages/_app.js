1ec4f065f30a42881d2f807da428080a1090b356
Chris Grabiński <chris@remote.com>
2021-03-09 23:39:06 +0000

    fix: remove scrolling issues on route change

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,31 @@
 import "tailwindcss/tailwind.css";
-import { Global, css } from "@emotion/react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 import Head from "components/Head";
 
+// ==================================================
+// Fix for smooth scrolling during route changes
+// GitHub: https://github.com/vercel/next.js/issues/20125
+// ==================================================
+function useNormalScrollRoutes() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => {
+      document.documentElement.style.scrollBehavior = "auto";
+    });
+    router.events.on("routeChangeComplete", () => {
+      document.documentElement.style.scrollBehavior = "smooth";
+    });
+  }, []);
+}
+
 function MyApp({ Component, pageProps }) {
+  useNormalScrollRoutes();
+
   return (
     <>
-      <Global
-        styles={css`
-          html {
-            scroll-behavior: smooth;
-          }
-        `}
-      />
       <Head />
       <Component {...pageProps} />
     </>
