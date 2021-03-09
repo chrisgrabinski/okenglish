0572434e27c1d264203ad57b6550427ac657870b
Chris Grabiński <chris@remote.com>
2021-03-09 23:20:05 +0000

    chore: adjusted card

--- components/views/video/BreakfastCard/BreakfastCard.js
+++ components/views/video/BreakfastCard/BreakfastCard.js
@@ -15,21 +15,34 @@ export default function BreakfastCard() {
   const isMonday = currentDay === 1;
   const isWithinHours = currentHours >= startTime && currentHours <= endTime;
 
-  const isLive = true || (isMonday && isWithinHours);
+  const isLive = isMonday && isWithinHours;
 
-  console.log(currentDate, currentDay, currentHours, startTime, endTime);
+  const buttonLabel = !isLive ? "Subskrybuj teraz" : "Obejrzyj teraz";
 
   return (
     <Card isHighlight>
-      {isLive && <LiveIndicator />}
-      <h3 className="mt-4">
+      {isLive && (
+        <div className="mb-4">
+          <LiveIndicator />
+        </div>
+      )}
+      <h3>
         <span className="block font-bold text-3xl">English Breakfast</span>
         <span className="block text-2xl italic">with&nbsp;Joanna</span>
       </h3>
       <p className="mt-4 text-lg">
         Tune in every Monday morning from 9-10 on Facebook Live
       </p>
-      <div className="mt-8">Obejrzyj teraz →</div>
+      <div className="mt-8">
+        <a
+          href="https://www.facebook.com/okenglish.eu"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {buttonLabel} →
+        </a>
+      </div>
+
       <div className="mt-8 -mx-8 -mb-8">
         <Image
           src="/images/ok-english-breakfast-teaser.png"
